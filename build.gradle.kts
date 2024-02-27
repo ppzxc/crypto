6a834749c5a3f66cedaf07f330c00289b8cc3e69
cjh8487 <ppzxc8487@gmail.com>
2024-02-27 14:39:35 +0900

    2024-02-27 feature/add action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+object Metadata {
+    const val description = "Encryption/Decryption Wrapper And Provide Simple Usability."
+    const val license = "MIT License"
+    const val licenseUrl = "https://opensource.org/license/mit"
+    const val githubRepo = "ppzxc/crypto"
+    const val release = "https://s01.oss.sonatype.org/service/local/"
+    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
+
 plugins {
-    java
+    `java-library`
     id("me.champeau.jmh") version "0.7.2"
     id("maven-publish")
     signing
@@ -13,8 +22,15 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+val repositories = arrayOf(
+    "https://oss.sonatype.org/content/repositories/snapshots/",
+    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+)
+
 repositories {
+    mavenLocal()
     mavenCentral()
+    repositories.forEach { maven(it) }
 }
 
 dependencies {
@@ -33,51 +49,6 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-fun jmh() {
-//    includes = ['some regular expression'] // include pattern (regular expression) for benchmarks to be executed
-//    excludes = ['some regular expression'] // exclude pattern (regular expression) for benchmarks to be executed
-//    iterations = 10 // Number of measurement iterations to do.
-//    benchmarkMode = ['thrpt','ss'] // Benchmark mode. Available modes are: [Throughput/thrpt, AverageTime/avgt, SampleTime/sample, SingleShotTime/ss, All/all]
-//    batchSize = 1 // Batch size: number of benchmark method calls per operation. (some benchmark modes can ignore this setting)
-//    fork = 2 // How many times to forks a single benchmark. Use 0 to disable forking altogether
-//    failOnError = false // Should JMH fail immediately if any benchmark had experienced the unrecoverable error?
-//    forceGC = false // Should JMH force GC between iterations?
-//    jvm = 'myjvm' // Custom JVM to use when forking.
-//    jvmArgs = ['Custom JVM args to use when forking.']
-//    jvmArgsAppend = ['Custom JVM args to use when forking (append these)']
-//    jvmArgsPrepend =[ 'Custom JVM args to use when forking (prepend these)']
-//    humanOutputFile = project.file("${project.buildDir}/reports/jmh/human.txt") // human-readable output file
-//    resultsFile = project.file("${project.buildDir}/reports/jmh/results.txt") // results file
-//    operationsPerInvocation = 10 // Operations per invocation.
-//    benchmarkParameters =  [:] // Benchmark parameters.
-//    profilers = [] // Use profilers to collect additional data. Supported profilers: [cl, comp, gc, stack, perf, perfnorm, perfasm, xperf, xperfasm, hs_cl, hs_comp, hs_gc, hs_rt, hs_thr, async]
-//    timeOnIteration = '1s' // Time to spend at each measurement iteration.
-//    resultFormat = 'CSV' // Result format type (one of CSV, JSON, NONE, SCSV, TEXT)
-//    synchronizeIterations = false // Synchronize iterations?
-//    threads = 4 // Number of worker threads to run with.
-//    threadGroups = [2,3,4] //Override thread group distribution for asymmetric benchmarks.
-//    jmhTimeout = '1s' // Timeout for benchmark iteration.
-//    timeUnit = 'ms' // Output time unit. Available time units are: [m, s, ms, us, ns].
-//    verbosity = 'NORMAL' // Verbosity mode. Available modes are: [SILENT, NORMAL, EXTRA]
-//    warmup = '1s' // Time to spend at each warmup iteration.
-//    warmupBatchSize = 10 // Warmup batch size: number of benchmark method calls per operation.
-//    warmupForks = 0 // How many warmup forks to make for a single benchmark. 0 to disable warmup forks.
-//    warmupIterations = 1 // Number of warmup iterations to do.
-//    warmupMode = 'INDI' // Warmup mode for warming up selected benchmarks. Warmup modes are: [INDI, BULK, BULK_INDI].
-//    warmupBenchmarks = ['.*Warmup'] // Warmup benchmarks to include in the run in addition to already selected. JMH will not measure these benchmarks, but only use them for the warmup.
-//
-//    zip64 = true // Use ZIP64 format for bigger archives
-//    jmhVersion = '1.37' // Specifies JMH version
-//    includeTests = true // Allows to include test sources into generate JMH jar, i.e. use it when benchmarks depend on the test classes.
-//    duplicateClassesStrategy = DuplicatesStrategy.FAIL // Strategy to apply when encountring duplicate classes during creation of the fat jar (i.e. while executing jmhJar task)
-}
-
-tasks {
-    jar {
-
-    }
-}
-
 java {
     withSourcesJar()
     withJavadocJar()
@@ -89,24 +60,35 @@ artifacts {
 }
 
 signing {
-    sign(configurations.archives.name)
+    val signingKey = providers
+        .environmentVariable("GPG_SIGNING_KEY")
+    val signingPassphrase = providers
+        .environmentVariable("GPG_SIGNING_PASSPHRASE")
+
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions
+            .getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    }
 }
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
             pom {
-                name = "crypto"
-                description = "Encryption/Decryption Wrapper And Provide Simple Usability."
-                url = "https://github.com/ppzxc/crypto"
-                groupId = "io.github.ppzxc"
-                artifactId = "crypto"
-                version = "0.0.11"
+                name = project.name
+                description = Metadata.description
+                url = "https://github.com/${Metadata.githubRepo}"
 
                 licenses {
                     license {
-                        name = "MIT License"
-                        url = "https://opensource.org/license/mit"
+                        name = Metadata.license
+                        url = Metadata.licenseUrl
                     }
                 }
 
@@ -119,9 +101,13 @@ publishing {
                 }
 
                 scm {
-                    connection = "scm:git:git://github.com/ppzxc/crypto.git"
-                    developerConnection = "scm:git:ssh://github.com/ppzxc/crypto.git"
-                    url = "https://github.com/ppzxc/crypto"
+                    connection = "scm:git:git://github.com/${Metadata.githubRepo}.git"
+                    developerConnection = "scm:git:ssh://github.com/${Metadata.githubRepo}.git"
+                    url = "https://github.com/${Metadata.githubRepo}"
+                }
+
+                issueManagement {
+                    url.set("https://github.com/${Metadata.githubRepo}/issues")
                 }
             }
         }
