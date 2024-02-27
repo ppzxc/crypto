7dcbd30296e2215ce07ba5df41af03e90ae197fb
cjh8487 <ppzxc8487@gmail.com>
2024-02-27 18:11:58 +0900

    2024-02-27 release/0.0.24

--- README.md
+++ README.md
@@ -12,3 +12,9 @@
 ## Asymmetric
 
 - [X] RSA
+
+# Todo
+
+- [ ]  remove jmh.
+- [ ]  deploy 'central.sonatype.com' using 'net.thebugmc.gradle.sonatype-central-portal-publisher' plugin.
+- [ ]  Migration when the 'io.github.gradle-nexus.publish-plugin' plug-in becomes available to central.sonatype.com.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     `java-library`
-    `maven-publish`
     signing
-    id("me.champeau.jmh") version "0.7.2"
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.1.1"
 }
 
 group = providers.gradleProperty("GROUP_NAME").get()
@@ -45,70 +43,124 @@ artifacts {
 }
 
 signing {
-    val signingKey = providers.environmentVariable("GPG_SIGNING_KEY")
-    val signingPassphrase = providers.environmentVariable("GPG_SIGNING_PASSPHRASE")
+//    val signingKey = providers.environmentVariable("GPG_SIGNING_KEY")
+    val signingKey = """
+        -----BEGIN PGP PRIVATE KEY BLOCK-----
 
-    if (signingKey.isPresent && signingPassphrase.isPresent) {
-        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
-        val extension = extensions.getByName("publishing") as PublishingExtension
-        sign(extension.publications)
-    }
+        lIYEZd1yjRYJKwYBBAHaRw8BAQdAklsVjyBRYHHRaxMU55pNHUU5Q78um9iKt0b7
+        IvsjGw/+BwMCGUURIafvs/Du83N7yI+9rEQQfwvHuX9W4W3/bzWYYjmgjJENScZD
+        Yg0FCmAS3WkmFoxUopCX05WsmdxMsLaThcAUlvvMjoZ/QLoQNeobyrQZcHB6eGMg
+        PGNqaDg0ODdAbmF2ZXIuY29tPoiZBBMWCgBBFiEEKv/S0IpLwYc2TSUaAZQafitW
+        wTIFAmXdco0CGwMFCQWkyaMFCwkIBwICIgIGFQoJCAsCBBYCAwECHgcCF4AACgkQ
+        AZQafitWwTJsggD/YszcnNHSzriOJ5JKtLcZb0Yq9DuObP7v1zuMWYFE6D8A/3B8
+        IC8eB3RqUuom4ysYggK+BYlsJtIMGsJatWvVmFACnIsEZd1yjRIKKwYBBAGXVQEF
+        AQEHQAZRNiGRhXgLLnwKM2Zo4oB7ygUBw8XN1WHrcCHSC41hAwEIB/4HAwJYouov
+        JWT+mO77JJ4LQv0NOibJnytqsb2/xXGisjmLEPWK87KdzAqoH9txhIYMNkLq+zu/
+        pzpohPnQmqq5TeLXI5LIFjMIZgtZD7O8xrRNiH4EGBYKACYWIQQq/9LQikvBhzZN
+        JRoBlBp+K1bBMgUCZd1yjQIbDAUJBaTJowAKCRABlBp+K1bBMnXjAP9tD4bKaqTw
+        ee24G4IPADxbr7J3XQihgkA9R5ORfc0IggD/UPZUSyx6aqlJsNAJGMGoMtR1Z65Z
+        ohpGAaAwTaVoMAw=
+        =vlkt
+        -----END PGP PRIVATE KEY BLOCK-----
+    """.trimIndent()
+//    val signingPassphrase = providers.environmentVariable("GPG_SIGNING_PASSPHRASE")
+    val signingPassphrase = "asdf851216"
+
+//    if (signingKey.isPresent && signingPassphrase.isPresent) {
+    useInMemoryPgpKeys(signingKey, signingPassphrase)
+    val extension = extensions.getByName("publishing") as PublishingExtension
+    sign(extension.publications)
+//    }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            groupId = providers.gradleProperty("GROUP_NAME").get()
-            artifactId = providers.gradleProperty("ARTIFACT_NAME").get()
-            version = providers.gradleProperty("VERSION_NAME").get()
-
-            pom {
-                name = providers.gradleProperty("ARTIFACT_NAME").get()
-                description = providers.gradleProperty("POM_DESCRIPTION").get()
-                url = providers.gradleProperty("POM_URL").get()
-
-                licenses {
-                    license {
-                        name = providers.gradleProperty("POM_LICENSE_NAME").get()
-                        url = providers.gradleProperty("POM_LICENSE_URL").get()
-                    }
-                }
-
-                developers {
-                    developer {
-                        id = providers.gradleProperty("POM_DEVELOPER_ID").get()
-                        name = providers.gradleProperty("POM_DEVELOPER_NAME").get()
-                        email = providers.gradleProperty("POM_DEVELOPER_EMAIL").get()
-                        url = providers.gradleProperty("POM_DEVELOPER_URL").get()
-                    }
-                }
-
-                scm {
-                    url = providers.gradleProperty("POM_SCM_URL").get()
-                    connection = providers.gradleProperty("POM_SCM_CONNECTION").get()
-                    developerConnection = providers.gradleProperty("POM_SCM_DEV_CONNECTION").get()
-                }
-
-                issueManagement {
-                    url = providers.gradleProperty("POM_ISSUE_MANAGEMENT_URL").get()
-                }
+centralPortal {
+    username = "0wr4f6mf"
+    password = "8xOqwiPfZREm4uW4x5eY/kJ2jK7YUqyJ5JtMb3qAH2RN"
+    pom {
+        group = providers.gradleProperty("GROUP_NAME").get()
+        name = providers.gradleProperty("ARTIFACT_NAME").get()
+        description = providers.gradleProperty("POM_DESCRIPTION").get()
+        packaging = "jar"
+        // `name = project.name`, `description = project.description` and `packaging = "jar"`
+        // are applied automatically, but you can override them here
+        url = providers.gradleProperty("POM_URL").get()
+        licenses {
+            license {
+                name = providers.gradleProperty("POM_LICENSE_NAME").get()
+                url = providers.gradleProperty("POM_LICENSE_URL").get()
             }
         }
-    }
-}
-
-nexusPublishing {
-    repositories {
-//        sonartype()
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://central.sonatype.com/service/local/staging/deploy/maven2/"))
-//            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/content/repositories/snapshots/"))
-            username = "0wr4f6mf"
-            password = "8xOqwiPfZREm4uW4x5eY/kJ2jK7YUqyJ5JtMb3qAH2RN"
+        developers {
+            developer {
+                id = providers.gradleProperty("POM_DEVELOPER_ID").get()
+                name = providers.gradleProperty("POM_DEVELOPER_NAME").get()
+                email = providers.gradleProperty("POM_DEVELOPER_EMAIL").get()
+                url = providers.gradleProperty("POM_DEVELOPER_URL").get()
+            }
+        }
+        scm {
+            url = providers.gradleProperty("POM_SCM_URL").get()
+            connection = providers.gradleProperty("POM_SCM_CONNECTION").get()
+            developerConnection = providers.gradleProperty("POM_SCM_DEV_CONNECTION").get()
         }
     }
 }
+
+
+//publishing {
+//    publications {
+//        create<MavenPublication>("mavenJava") {
+//            groupId = providers.gradleProperty("GROUP_NAME").get()
+//            artifactId = providers.gradleProperty("ARTIFACT_NAME").get()
+//            version = providers.gradleProperty("VERSION_NAME").get()
+//
+//            pom {
+//                name = providers.gradleProperty("ARTIFACT_NAME").get()
+//                description = providers.gradleProperty("POM_DESCRIPTION").get()
+//                url = providers.gradleProperty("POM_URL").get()
+//
+//                licenses {
+//                    license {
+//                        name = providers.gradleProperty("POM_LICENSE_NAME").get()
+//                        url = providers.gradleProperty("POM_LICENSE_URL").get()
+//                    }
+//                }
+//
+//                developers {
+//                    developer {
+//                        id = providers.gradleProperty("POM_DEVELOPER_ID").get()
+//                        name = providers.gradleProperty("POM_DEVELOPER_NAME").get()
+//                        email = providers.gradleProperty("POM_DEVELOPER_EMAIL").get()
+//                        url = providers.gradleProperty("POM_DEVELOPER_URL").get()
+//                    }
+//                }
+//
+//                scm {
+//                    url = providers.gradleProperty("POM_SCM_URL").get()
+//                    connection = providers.gradleProperty("POM_SCM_CONNECTION").get()
+//                    developerConnection = providers.gradleProperty("POM_SCM_DEV_CONNECTION").get()
+//                }
+//
+//                issueManagement {
+//                    url = providers.gradleProperty("POM_ISSUE_MANAGEMENT_URL").get()
+//                }
+//            }
+//        }
+//    }
+//}
+
+//nexusPublishing {
+//    repositories {
+////        sonartype()
+//        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+//            nexusUrl.set(uri("https://central.sonatype.com/service/local/staging/deploy/maven2/"))
+////            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+//            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/content/repositories/snapshots/"))
+//            username = "0wr4f6mf"
+//            password = "8xOqwiPfZREm4uW4x5eY/kJ2jK7YUqyJ5JtMb3qAH2RN"
+//        }
+//    }
+//}
 
 //publishing {
 //    repositories {
